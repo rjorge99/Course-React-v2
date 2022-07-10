84307421a9750d0d75a2a0ee1b7c5ffe4223452e
Jorge Reyes <rjorge99@hotmail.com>
2022-07-09 23:39:12 -0500

    Se agrega config para que vite no marque error al ejecutar

--- 08-journal-app/vite.config.js
+++ 08-journal-app/vite.config.js
@@ -6,5 +6,10 @@ export default defineConfig({
     plugins: [react()],
     server: {
         open: true
+    },
+    esbuild: {
+        define: {
+            this: 'window'
+        }
     }
 });
