e27be49a10887658dd4e45f1cd088a505127f39b
Jorge Reyes <rjorge99@hotmail.com>
2022-07-15 11:53:08 -0500

    Se agrega calendario

--- 10-calendar/src/calendar/pages/CalendarPage.jsx
+++ 10-calendar/src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,43 @@
 import { NavBar } from '../components/NavBar';
 
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import enUS from 'date-fns/locale/en-US';
+import { addHours, format, parse, startOfWeek, getDay } from 'date-fns';
+
+const locales = {
+    'en-US': enUS
+};
+
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales
+});
+
+const events = [
+    {
+        title: 'All Day Event',
+        notes: 'Notas',
+        start: new Date(),
+        end: addHours(new Date(), 2)
+    }
+];
+
 export const CalendarPage = () => {
     return (
         <>
             <NavBar />
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor='start'
+                endAccessor='end'
+                style={{ height: 'calc(100vh - 80px)' }}
+            />
         </>
     );
 };
