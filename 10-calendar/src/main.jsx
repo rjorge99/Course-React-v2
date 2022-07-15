eadbfc3531531d35ca5a66e935ee9c1f64b1ed17
Jorge Reyes <rjorge99@hotmail.com>
2022-07-15 11:40:19 -0500

    Se agrega navbar y pantalla de login

--- 10-calendar/src/main.jsx
+++ 10-calendar/src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { CalendarApp } from './CalendarApp';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <React.StrictMode>
+        <CalendarApp />
+    </React.StrictMode>
+);
---
+++ 10-calendar/src/router/AppRouter.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import { LoginPage } from '../auth/pages/LoginPage';
+import { CalendarPage } from '../calendar/pages/CalendarPage';
+
+export const AppRouter = () => {
+    const authStatus = 'authenticated';
+
+    return (
+        <Routes>
+            {authStatus === 'not-authenticated' ? (
+                <Route path='/auth/*' element={<LoginPage />} />
+            ) : (
+                <Route path='/*' element={<CalendarPage />} />
+            )}
+        </Routes>
+    );
+};
